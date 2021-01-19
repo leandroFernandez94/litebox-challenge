0d38666f4550e3a899229f04487bf7dace9fa641
Leandro Fernandez <leandroofernandezz@gmail.com>
2021-01-19 17:24:46 -0300

    add poster hovers

--- components/MoviesSection/MoviePoster.js
+++ components/MoviesSection/MoviePoster.js
@@ -1,10 +1,26 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import classNames from 'classnames'
 import styles from './MoviesSection.module.css'
 
 export default function MoviePoster({movie, usePoster}) {
   const [loadingPoster, setLoadingPoster] = useState(true)
   const [poster, setPoster] = useState(null)
+  const [isHovered, setIsHovered] = useState(false)
+  const containerRef = useRef()
+
+  useEffect(downloadPoster, [])
+
+  
+  useEffect(() => {
+    if(loadingPoster) return
+    containerRef.current.addEventListener('mouseenter', () => {
+      setIsHovered(true)
+    })
+    containerRef.current.addEventListener('mouseleave', () => {
+      setIsHovered(false)
+    })
+  }, [loadingPoster])
+
 
   async function downloadPoster() {
     const posterUrl = movie.posterUrl || `https://image.tmdb.org/t/p/w500${usePoster ? movie.poster_path : movie.backdrop_path}`
@@ -14,13 +30,20 @@ export default function MoviePoster({movie, usePoster}) {
     setLoadingPoster(false)
   }
 
-  useEffect(downloadPoster, [])
   
   return loadingPoster 
     ? 'Loading poster'
     : (
-      <div className={styles.posterContainer}>
-        <img src={poster} className={styles.posterImg}/>
+      <div ref={containerRef} className={styles.posterContainer}>
+        <div className={styles.imageContainer}>
+          <img src={poster} className={styles.posterImg}/>
+        </div>
+        <div className={styles.arrowContainer}>
+          <img className={styles.posterHoverArrow} src="/arrow-poster-hover.svg"></img>
+        </div>
+        <div className={styles.playButton}>
+          <img src="/play.svg"/>
+        </div>
       </div>
     ) 
 }
